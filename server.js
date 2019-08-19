013a684067a9100dec7970f424b2ada98a94e71d
williammb <wmb96@hotmail.com>
2019-08-19 13:32:16 -0300

    works

--- server.js
+++ server.js
@@ -2,22 +2,27 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
+    // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/', function(req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.get('/hello', function(req, res) {
+    res.json({ greetings: "Hello, API" });
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next) {
+    res.json({ name: req.file.originalname, size: req.file.size });
+})
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Node.js listening ...');
 });
